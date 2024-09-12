25338b4243f14e80822e402f50246708d29f423a
virgiseptian77@gmail.com <94891367+virgi-septian@users.noreply.github.com>
2024-09-12 18:33:30 +0700

    login user

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -7,6 +8,16 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              guard let strongSelf = self else { return }
+                if let e = error {
+                    print(e)
+                } else {
+                    self?.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
