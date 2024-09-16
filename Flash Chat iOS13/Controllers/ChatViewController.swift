d09c64babc1620089bb48c3dfe77ac9480dec1b4
virgiseptian77@gmail.com <94891367+virgi-septian@users.noreply.github.com>
2024-09-16 20:04:16 +0700

    Use a tableview and creating a Message model

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -6,9 +6,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var message: [Message] = [
+        Message(sender: "1@2gmail.com", body: "Hey !"),
+        Message(sender: "a@bgmail.com", body: "Hello!"),
+        Message(sender: "1@2gmail.com", body: "What's up")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "⚡️FlashChat"
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
     }
     
@@ -25,4 +32,18 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+//MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(message[indexPath.row].body)"
+        
+        return cell
+    }
 }
