cfc68b8bdaf13e98f1a16a9f04f820d795a0ca13
virgiseptian77@gmail.com <94891367+virgi-septian@users.noreply.github.com>
2024-09-18 10:19:37 +0700

    Table view scroll to bottom

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -38,6 +38,8 @@ class ChatViewController: UIViewController {
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
                                 
+                                let indexPath = IndexPath(row: self.message.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
